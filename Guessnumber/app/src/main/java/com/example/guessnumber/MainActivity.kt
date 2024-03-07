7eb7ce90211033a053212b36c8c9954a996d220b
wweric13 <weibaby2003@gmail.com>
2024-03-07 16:55:23 +0800

    init2

--- Guessnumber/app/src/main/java/com/example/guessnumber/MainActivity.kt
+++ Guessnumber/app/src/main/java/com/example/guessnumber/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.guessnumber
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
@@ -10,9 +12,12 @@ import java.util.*
 
 
 class MainActivity : AppCompatActivity() {
+    val TAG:String = MainActivity::class.java.simpleName
+    private lateinit var handler:Handler
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        handler = Handler(Looper.getMainLooper())
 
         val textView = findViewById<TextView>(R.id.EditText)
         val result_textView=findViewById<TextView>(R.id.result_textView)
@@ -24,20 +29,28 @@ class MainActivity : AppCompatActivity() {
         val secret : Int = Random().nextInt(10)+1
         guess_button.setOnClickListener{
             result_textView.text=editText.text
-           //Toast.makeText(this,editText.text,Toast.LENGTH_LONG)
+            handler.postDelayed({
+                Toast.makeText(this,"5秒後操作執行了!",Toast.LENGTH_SHORT).show()
+            },5000)
+
             validate_num =editText.text.toString().toInt()-secret
             var ans_str:String="BINGO"
             if (validate_num>0){
-                ans_str="你猜的比較大喔"
+                ans_str="Answer_is_smaller"
             }else if(validate_num<0){
-                ans_str="你猜得比較小喔"
+                ans_str="Answer_is_bigger"
             }
             result_textView.text=ans_str
         }
         reset_button.setOnClickListener{
-            validate_num = Random().nextInt(10)+1
-            result_textView.text= "再猜一次"
+            validate_num=Random().nextInt(10)+1
+            result_textView.text= "One_more_time"
 
         }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        handler.removeCallbacksAndMessages(null)
     }
 }
